424edd4417f486df0b5ef74eede3d35049962106
LonelySakura <47115548+LonelySakura@users.noreply.github.com>
2019-11-15 14:46:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
-    print("new git")
+    print("print(print(print(print(print(print(print(print(print(print(print(print(print(print(print())))))))))))))))")
+    print("Hto ya)")
 
 
 if __name__ == "__main__":
